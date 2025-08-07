3420a7fda2d2023c5a2a451dcd08e2680a7dcf18
Isaac Wu <isaacwu2000@gmail.com>
2025-08-07 11:21:36 -0400

    optimized pathfinder by not checking paths that are already above the minimum cost, and having minimum cost be intitialized as the diagonal

--- src/Main.java
+++ src/Main.java
@@ -2,84 +2,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /* Citation: I used the Google AI Search Overview occasionally for syntax. - Isaac Wu, August 6th, 2025 */
-
 // Note: 1=right, 2=down, 3=diagonal
 public class Main {
-    private static int n = 3;
+    private static int n = 4;
     private static int[][] board = new int[n][n];
-    private static ArrayList<ArrayList<Integer>> traversions = new ArrayList<ArrayList<Integer>>();
+    private static ArrayList<Integer> cheapestTraversion = new ArrayList<Integer>();
+    private static int minCost;
 
     private static void FillBoard() {
         for (int i=0; i<n; i++) {for (int j=0; j<n; j++) {board[i][j] = (int)(101*Math.random());}}
     }
 
-    private static int[] getPosition(ArrayList<Integer> moves) {
-        int right = 0; int down = 0;
-        for (int move : moves) {
-            if (move==1) {right+=1;}
-            else if (move==2) {down+=1;}
-            else {right+=1; down+=1;}
-        }
-        int[] position = {right, down};
-        return position;
-    }
-
-    private static void TraverseBoard(ArrayList<Integer> previousMoves) {
-        int[] currentPosition = getPosition(previousMoves);
-        ArrayList<Integer> moves = (ArrayList<Integer>) previousMoves.clone(); // Making a clone prevents the list from getting moves from other calls
-
-        if (currentPosition[0]==n-1 && currentPosition[1]==n-1) { // Adding the moves when we get to the end
-            traversions.add(moves);
-        }
-        else if (currentPosition[0]==n-1) { // When we are at the right edge, we only go down
-            for (int i=0; i<n-1-currentPosition[1]; i++) {
+    private static void TraverseBoard(int cost, int right, int down, ArrayList<Integer> previousMoves) {
+        if (cost < minCost) {
+            if (right==n-1 && down==n-1) {
+                cheapestTraversion = (ArrayList<Integer>) previousMoves.clone();
+                minCost = cost;
+            } else if (right == n-1) {
+                ArrayList<Integer> moves = (ArrayList<Integer>) previousMoves.clone();
                 moves.add(2);
-            }
-            traversions.add(moves);
-        }
-        else if (currentPosition[1]==n-1) { // When we are at the bottom edge, we only go right
-            for (int i=0; i<n-1-currentPosition[0]; i++) {
+                TraverseBoard(cost+board[down+1][right], right, down+1, moves);
+            } else if (down == n-1) {
+                ArrayList<Integer> moves = (ArrayList<Integer>) previousMoves.clone();
                 moves.add(1);
-            }
-            traversions.add(moves);
-        }
-        else { // Doing all valid moves (right, down, diagonal)
-            moves.add(1);
-            TraverseBoard(moves);
-            moves = (ArrayList<Integer>) previousMoves.clone();
-            moves.add(2);
-            TraverseBoard(moves);
-            moves = (ArrayList<Integer>) previousMoves.clone();
-            moves.add(3);
-            TraverseBoard(moves);
-        }
-    }
+                TraverseBoard(cost+board[down][right+1], right+1, down, moves);
+            } else {
+                ArrayList<Integer> moves = (ArrayList<Integer>) previousMoves.clone();
+                moves.add(3); // Doing diagonal first, which will be closer to the average cheapest path
+                TraverseBoard(cost+board[down+1][right+1], right+1, down+1, moves);
 
-    private static int getCost(ArrayList<Integer> traversion) {
-        int cost = board[0][0];
-        for (int i=0; i<traversion.size(); i++) {
-            // Cutting the portion of the traversion to get the square
-            ArrayList<Integer> cutTraversion = new ArrayList<Integer>();
-            for (int m=0; m<i+1; m++) {cutTraversion.add(traversion.get(m));}
-            int[] position = getPosition(cutTraversion);
-            cost += board[position[1]][position[0]];
+                moves = (ArrayList<Integer>) previousMoves.clone();
+                moves.add(2);
+                TraverseBoard(cost+board[down+1][right], right, down+1, moves);
+
+                moves = (ArrayList<Integer>) previousMoves.clone();
+                moves.add(1);
+                TraverseBoard(cost+board[down][right+1], right+1, down, moves);
+            }
         }
-        return cost;
     }
 
     public static void main(String[] args) {
         FillBoard();
         for (int[] row : board) {System.out.println(Arrays.toString(row));}
-
-        TraverseBoard(new ArrayList<Integer>());
-
-        int minCost = getCost(traversions.get(0));
-        ArrayList<Integer> minCostPath = traversions.get(0);
-        for (ArrayList<Integer> t : traversions) {
-            int cost = getCost(t);
-            if (cost < minCost) {minCost = cost; minCostPath=t;}
+        // Initializing minCost and cheapestTraversion to be that of the diagonal (shortest path)
+        minCost = board[0][0];
+        for (int i=1; i<n; i++) {
+            minCost += board[i][i];
+            cheapestTraversion.add(3);
         }
-        System.out.println("Minimum Cost: " + minCost);
-        System.out.println("Minimum Cost Path: " + minCostPath);
+
+        TraverseBoard(board[0][0], 0, 0, new ArrayList<Integer>());
+        System.out.println("\nMin Cost: " + minCost);
+        System.out.println("Cheapest Traversion: " + cheapestTraversion);
     }
 }
