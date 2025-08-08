ccdd77a2a194df581d451ecdcdfdf60985caef5e
Isaac Wu <isaacwu2000@gmail.com>
2025-08-08 15:06:57 -0400

    prevented position repeats. Finished pathfinder program with 8 directions

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 // Lower numbers are done first because they are more likely to be optimal
 
 public class Main {
-    private static int n = 3;
+    private static int n = 7;
     private static int[][] board = new int[n][n];
     private static ArrayList<Integer> cheapestTraversion = new ArrayList<Integer>();
     private static int minCost;
@@ -20,133 +20,148 @@ public class Main {
     private static void FillBoard() {
         for (int i=0; i<n; i++) {for (int j=0; j<n; j++) {board[i][j] = (int)(101*Math.random());}}
     }
+
+    private static boolean PositionRepeats(int positionRight, int positionDown, ArrayList<Integer> moves) {
+        int right = 0;
+        int down = 0;
+        for (int i=0; i<moves.size()-1; i++) {
+            int move = moves.get(i);
+            if (move==1) {right+=1; down+=1;}
+            else if (move==2) {down+=1;}
+            else if (move==3) {right+=1;}
+            else if (move==4) {right-=1; down+=1;}
+            else if (move==5) {right+=1; down-=1;}
+            else if (move==6) {right-=1;}
+            else if (move==7) {down-=1;}
+            else {right-=1; down-=1;}
+            if (right==positionRight && down==positionDown) {return true;}
+        }
+        return false;
+    }
     // todo: check if the move lands on a place you've already gone to
-    // todo: only do moves that are possible
     private static void TraverseBoard(int cost, int right, int down, ArrayList<Integer> previousMoves) {
         if (cost < minCost) {
-            ArrayList<Integer> moves = (ArrayList<Integer>) previousMoves.clone();
-            if (right==n-1 && down==n-1) { // Bottom right corner (path complete)
-                cheapestTraversion = moves;
-                minCost = cost;
-            } else if (right==0 && down==0){ // Top left corner
-                moves.add(1); // ↘
-                TraverseBoard(cost+board[down+1][right+1], right+1, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(2); // ↓
-                TraverseBoard(cost+board[down+1][right], right, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(3); // →
-                TraverseBoard(cost+board[down][right+1], right+1, down, moves);
-            } else if (right==0 && down==n-1) { // Bottom left corner
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(3); // →
-                TraverseBoard(cost+board[down][right+1], right+1, down, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(5); // ↗
-                TraverseBoard(cost+board[down-1][right+1], right+1, down-1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(7); // ↑
-                TraverseBoard(cost+board[down-1][right], right, down-1, moves);
-            } else if (right==n-1 && down == 0) { // Top right corner
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(2); // ↓
-                TraverseBoard(cost+board[down+1][right], right, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(4); // ↙
-                TraverseBoard(cost+board[down+1][right-1], right-1, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(6); // ←
-                TraverseBoard(cost+board[down][right-1], right-1, down, moves);
-            } else if (down==n-1) { // Bottom edge
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(3); // →
-                TraverseBoard(cost+board[down][right+1], right+1, down, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(5); // ↗
-                TraverseBoard(cost+board[down-1][right+1], right+1, down-1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(6); // ←
-                TraverseBoard(cost+board[down][right-1], right-1, down, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(7); // ↑
-                TraverseBoard(cost+board[down-1][right], right, down-1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(8); // ↖
-                TraverseBoard(cost+board[down-1][right-1], right-1, down-1, moves);
-            } else if (right==n-1) { // Right edge
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(2); // ↓
-                TraverseBoard(cost+board[down+1][right], right, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(4); // ↙
-                TraverseBoard(cost+board[down+1][right-1], right-1, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(6); // ←
-                TraverseBoard(cost+board[down][right-1], right-1, down, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(7); // ↑
-                TraverseBoard(cost+board[down-1][right], right, down-1, moves);
-            } else if (right==0) { // Left edge
-                moves.add(1); // ↘
-                TraverseBoard(cost+board[down+1][right+1], right+1, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(2); // ↓
-                TraverseBoard(cost+board[down+1][right], right, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(3); // →
-                TraverseBoard(cost+board[down][right+1], right+1, down, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(5); // ↗
-                TraverseBoard(cost+board[down-1][right+1], right+1, down-1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(7); // ↑
-                TraverseBoard(cost+board[down-1][right], right, down-1, moves);
-            } else if (down==0) { // Top edge
-                moves.add(1); // ↘
-                TraverseBoard(cost+board[down+1][right+1], right+1, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(2); // ↓
-                TraverseBoard(cost+board[down+1][right], right, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(3); // →
-                TraverseBoard(cost+board[down][right+1], right+1, down, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(4); // ↙
-                TraverseBoard(cost+board[down+1][right-1], right-1, down+1, moves);
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(6); // ←
-                TraverseBoard(cost+board[down][right-1], right-1, down, moves);
-            } else { // Not along any edge
-                moves.add(1); // ↘
-                TraverseBoard(cost+board[down+1][right+1], right+1, down+1, moves);
-
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(2); // ↓
-                TraverseBoard(cost+board[down+1][right], right, down+1, moves);
-
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(3); // →
-                TraverseBoard(cost+board[down][right+1], right+1, down, moves);
-
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(4); // ↙
-                TraverseBoard(cost+board[down+1][right-1], right-1, down+1, moves);
-
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(5); // ↗
-                TraverseBoard(cost+board[down-1][right+1], right+1, down-1, moves);
-
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(6); // ←
-                TraverseBoard(cost+board[down][right-1], right-1, down, moves);
-
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(7); // ↑
-                TraverseBoard(cost+board[down-1][right], right, down-1, moves);
-
-                moves = (ArrayList<Integer>) previousMoves.clone();
-                moves.add(8); // ↖
-                TraverseBoard(cost+board[down-1][right-1], right-1, down-1, moves);
+            if (!PositionRepeats(right, down, previousMoves)) {
+                ArrayList<Integer> moves = (ArrayList<Integer>) previousMoves.clone();
+                if (right==n-1 && down==n-1) { // Bottom right corner (path complete)
+                    cheapestTraversion = moves;
+                    minCost = cost;
+                } else if (right==0 && down==0){ // Top left corner
+                    moves.add(1); // ↘
+                    TraverseBoard(cost+board[down+1][right+1], right+1, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(2); // ↓
+                    TraverseBoard(cost+board[down+1][right], right, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(3); // →
+                    TraverseBoard(cost+board[down][right+1], right+1, down, moves);
+                } else if (right==0 && down==n-1) { // Bottom left corner
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(3); // →
+                    TraverseBoard(cost+board[down][right+1], right+1, down, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(5); // ↗
+                    TraverseBoard(cost+board[down-1][right+1], right+1, down-1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(7); // ↑
+                    TraverseBoard(cost+board[down-1][right], right, down-1, moves);
+                } else if (right==n-1 && down == 0) { // Top right corner
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(2); // ↓
+                    TraverseBoard(cost+board[down+1][right], right, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(4); // ↙
+                    TraverseBoard(cost+board[down+1][right-1], right-1, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(6); // ←
+                    TraverseBoard(cost+board[down][right-1], right-1, down, moves);
+                } else if (down==n-1) { // Bottom edge
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(3); // →
+                    TraverseBoard(cost+board[down][right+1], right+1, down, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(5); // ↗
+                    TraverseBoard(cost+board[down-1][right+1], right+1, down-1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(6); // ←
+                    TraverseBoard(cost+board[down][right-1], right-1, down, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(7); // ↑
+                    TraverseBoard(cost+board[down-1][right], right, down-1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(8); // ↖
+                    TraverseBoard(cost+board[down-1][right-1], right-1, down-1, moves);
+                } else if (right==n-1) { // Right edge
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(2); // ↓
+                    TraverseBoard(cost+board[down+1][right], right, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(4); // ↙
+                    TraverseBoard(cost+board[down+1][right-1], right-1, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(6); // ←
+                    TraverseBoard(cost+board[down][right-1], right-1, down, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(7); // ↑
+                    TraverseBoard(cost+board[down-1][right], right, down-1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(8); // ↖
+                    TraverseBoard(cost+board[down-1][right-1], right-1, down-1, moves);
+                } else if (right==0) { // Left edge
+                    moves.add(1); // ↘
+                    TraverseBoard(cost+board[down+1][right+1], right+1, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(2); // ↓
+                    TraverseBoard(cost+board[down+1][right], right, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(3); // →
+                    TraverseBoard(cost+board[down][right+1], right+1, down, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(5); // ↗
+                    TraverseBoard(cost+board[down-1][right+1], right+1, down-1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(7); // ↑
+                    TraverseBoard(cost+board[down-1][right], right, down-1, moves);
+                } else if (down==0) { // Top edge
+                    moves.add(1); // ↘
+                    TraverseBoard(cost+board[down+1][right+1], right+1, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(2); // ↓
+                    TraverseBoard(cost+board[down+1][right], right, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(3); // →
+                    TraverseBoard(cost+board[down][right+1], right+1, down, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(4); // ↙
+                    TraverseBoard(cost+board[down+1][right-1], right-1, down+1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(6); // ←
+                    TraverseBoard(cost+board[down][right-1], right-1, down, moves);
+                } else { // Not along any edge
+                    moves.add(1); // ↘
+                    TraverseBoard(cost + board[down + 1][right + 1], right + 1, down + 1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(2); // ↓
+                    TraverseBoard(cost + board[down + 1][right], right, down + 1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(3); // →
+                    TraverseBoard(cost + board[down][right + 1], right + 1, down, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(4); // ↙
+                    TraverseBoard(cost + board[down + 1][right - 1], right - 1, down + 1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(5); // ↗
+                    TraverseBoard(cost + board[down - 1][right + 1], right + 1, down - 1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(6); // ←
+                    TraverseBoard(cost + board[down][right - 1], right - 1, down, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(7); // ↑
+                    TraverseBoard(cost + board[down - 1][right], right, down - 1, moves);
+                    moves = (ArrayList<Integer>) previousMoves.clone();
+                    moves.add(8); // ↖
+                    TraverseBoard(cost + board[down - 1][right - 1], right - 1, down - 1, moves);
+                }
             }
         }
     }
